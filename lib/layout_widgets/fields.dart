1ca313bf5c21b84e9c0748da393a22ce8c7b1ced
hem1t <sunny10fb@gmail.com>
2023-06-13 23:03:46 +0530

    counter field

--- lib/layout_widgets/fields.dart
+++ lib/layout_widgets/fields.dart
@@ -1,31 +1,75 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 import 'package:timers/color.dart';
 
-class CountDownField extends StatefulWidget {
-  final Duration? time;
-  const CountDownField({super.key, this.time});
+class CounterField extends StatelessWidget {
+  const CounterField({super.key});
 
-  @override
-  State<CountDownField> createState() => _CountDownFieldState();
-}
-
-class _CountDownFieldState extends State<CountDownField> {
   @override
   Widget build(BuildContext context) {
+    Duration time = context.watch<CounterFieldController>().current ?? 0.seconds;
     final style = TextStyle(
-        fontSize: 55.sp,
-        fontWeight: FontWeight.bold,
-        color:
-        widget.time == null ? AppColors.contrastColor : AppColors.mainHighlight);
-
-    Duration time =
-        widget.time ?? const Duration(hours: 0, minutes: 0, seconds: 0);
+      fontSize: 65.sp,
+      fontWeight: FontWeight.bold,
+      color:
+          time == 0.seconds ? AppColors.contrastColor : AppColors.mainHighlight,
+    );
 
     String tstring = time.toString();
     return Text(
       tstring.substring(0, tstring.indexOf('.')),
       style: style,
     );
+  }
+}
+
+class CounterFieldController extends ChangeNotifier {
+  Duration? time;
+  Duration? endTime;
+  Duration? current;
+  Timer? timer;
+  int stepSecs = 0;
+
+  prepare(Duration time, Duration endTime, int stepSecs) {
+    this.time = time;
+    this.endTime = endTime;
+    this.stepSecs = stepSecs;
+  }
+
+  start() {
+    current = time;
+    timer = Timer.periodic(stepSecs.seconds, (timer) {
+      if (current?.inSeconds == endTime?.inSeconds) {
+        notifyListeners();
+        timer.cancel();
+      } else {
+        current = (current ?? 0.seconds) + stepSecs.seconds;
+        notifyListeners();
+      }
+    });
+  }
+
+  pause() {
+    timer?.cancel();
+  }
+
+  resume() {
+    timer = Timer.periodic(stepSecs.seconds, (timer) {
+      if (current?.inSeconds == endTime?.inSeconds) {
+        notifyListeners();
+        timer.cancel();
+      } else {
+        current = (current ?? 0.seconds) + stepSecs.seconds;
+        notifyListeners();
+      }
+    });
+  }
+
+  reset() {
+    current = time;
   }
 }
